a1f98b88d206465e0e45632ff220c386955e8a7a
Simone Giordano <sg@simonegiordano.it>
2023-09-17 23:29:54 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Web.Administration;
+using System.Security.Cryptography.X509Certificates;
+using CommandLine;
+
+namespace Certbot2IIS
+{
+    internal class Program
+    {
+        public class Options
+        {
+            [Option('c', "certfile", Required = true, HelpText = "PEM certificate file")]
+            public string CertFile { get; set; } = "";
+
+            [Option('k', "keyfile", Required = true, HelpText = "PEM private key file")]
+            public string KeyFile { get; set; } = "";
+
+            [Option('n', "friendlyname", Required = true, HelpText = "Certificate friendly name")]
+            public string FriendlyName { get; set; } = "";
+
+            [Option('s', "sitename", Required = true, HelpText = "IIS site name")]
+            public string SiteName { get; set; } = "";
+
+            [Option('h', "hostname", Required = false, HelpText = "IIS site binding site name")]
+            public string HostName { get; set; } = "";
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(RunOptions);
+        }
+
+        static void RunOptions(Options opts)
+        { 
+            X509Certificate2 newCert = X509Certificate2.CreateFromPemFile(opts.CertFile, opts.KeyFile);
+            newCert.FriendlyName = opts.FriendlyName;
+            newCert = new X509Certificate2(newCert.Export(X509ContentType.Pfx), "", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+
+            X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadWrite);
+
+            bool certExists = false;
+            List<X509Certificate2> toRemove = new();
+            foreach (var c in store.Certificates)
+                if (c.FriendlyName.Equals(opts.FriendlyName, StringComparison.OrdinalIgnoreCase))
+                    if (c.GetCertHashString() != newCert.GetCertHashString())
+                        toRemove.Add(c);
+                    else
+                        certExists = true;
+
+            foreach (var c in toRemove)
+                store.Remove(c);
+
+            if (!certExists)
+                store.Add(newCert);
+
+            store.Close();
+
+            bool doCommit = false;
+            ServerManager serverManager = new ServerManager();
+            foreach (var b in serverManager.Sites[opts.SiteName].Bindings)
+            {
+                if (b.Protocol.ToLower() == "https")
+                {
+                    if ((opts.HostName.Length == 0) || b.Host.Equals(opts.HostName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (!b.CertificateHash.SequenceEqual(newCert.GetCertHash()))
+                        {
+                            b.CertificateStoreName = "My";
+                            b.CertificateHash = newCert.GetCertHash();
+                            doCommit = true;
+                        }
+                    }
+                }
+            }
+
+            if (doCommit)
+                serverManager.CommitChanges();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+﻿# Certbot2IIS
+Certbot2IIS is a tool to programmatically update IIS certificate generated 
+by Certbot.
+
+# Usage
+```
+Certbot2IIS 1.0.0
+Copyright (C) 2023 Brayns.it
+
+ERROR(S):
+  Required option 'c, certfile' is missing.
+  Required option 'k, keyfile' is missing.
+  Required option 'n, friendlyname' is missing.
+  Required option 's, sitename' is missing.
+
+  -c, --certfile        Required. PEM certificate file
+
+  -k, --keyfile         Required. PEM private key file
+
+  -n, --friendlyname    Required. Certificate friendly name
+
+  -s, --sitename        Required. IIS site name
+
+  -h, --hostname        IIS site binding site name
+
+  --help                Display this help screen.
+
+  --version             Display version information.
+```
+
+Example:
+```
+Certbot2IIS.exe 
+    -c C:\Certbot\live\mysite.mydomain.it\fullchain.pem 
+    -k C:\Certbot\live\mysite.mydomain.it\privkey.pem 
+    -n "Cert for mysite" 
+    -s "MYSITE" 
+    -h "mysite.mydomain.it"
+```
+
+# License
+Experience is an Open Source project, use it for free.
+
+# Credits
+Built with love :heart: in Italy by Brayns, an idea of Simone Giordano 
+[sg@simonegiordano.it](mailto:sg@simonegiordano.it)
